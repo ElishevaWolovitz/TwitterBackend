12472fd61d9570584c23c411e4d3bdeb79527cab
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-06 14:07:30 +0300

    completed user router

---
+++ TwitterService/src/user/user.router.ts
@@ -0,0 +1,26 @@
+import express, { Router } from 'express';
+import { 
+    controlCreateUser, 
+    controlReadAllUsers
+} from './user.controller';
+// import {
+//     createItemSchema,
+//     updateItemSchema,
+//     idParamSchema
+//   } from './item.validator';
+//   import { validateBody, validateParams } from '../joi.functions';
+
+
+const router: Router = express.Router();
+
+router.post(
+    '/newUser', 
+    //validateBody(createItemSchema), 
+    controlCreateUser
+);
+router.get(
+    '/allUsers', 
+    controlReadAllUsers
+);
+
+export default router;
