e908aa2356d0ddb20bde05a297e00878d6c93223
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-07 15:57:32 +0300

    refactored code so no unnamed functions and less repetition

--- TwitterService/src/tweet/tweet.controller.ts
+++ TwitterService/src/tweet/tweet.controller.ts
@@ -7,62 +7,50 @@ import {
     manageUpdateTweet, 
     manageDeleteTweet 
 } from "./tweet.manager";
-//import { handleError } from '../item.functions';
 import { TweetType } from "../../Lib/types/tweet.type";
+import {errorHandler} from '../../Lib/handler/error.handler';
+import {sucessHandler} from '../../Lib/handler/sucess.handler';
+
 
 // Create
 //Q: Do all functions in manager and repository also have to be async and await? or just here in controller? 
 //Q: Is the function inside the catch considered a annonymouse function and should be done differently? 
 export const controlCreateTweet = async (req: Request, res: Response) => {
     const tweet = new TweetModel(req.body as TweetType);
-    const createTweetResult = await manageCreateTweet(tweet).catch((error) => {
-        res.status(400).json({ error: error.message })});
+    const createTweetResult = await manageCreateTweet(tweet).catch(errorHandler(res, 400));
     if(createTweetResult)
-        res.status(201).json(createTweetResult);
+        sucessHandler(res, 'Created new tweet (in controller)', createTweetResult, 200);
 };
 
 // Read All
 export const controlReadAllTweets = async (req: Request, res: Response) => {
-    const tweets = await manageReadAllTweets().catch((error) => 
-        res.status(400).json({ error: error.message }));
+    const tweets = await manageReadAllTweets().catch(errorHandler(res, 400));
     if(tweets)
-        res.status(200).json(tweets);
-    else 
-        res.status(404).json({ message: "Tweets could not be read (in controlReadAllTweets)" });
+        sucessHandler(res, 'Read all tweets (in controller)', tweets, 200);
 };
 
 // Read One
 export const controlReadTweet = async (req: Request, res: Response) => {
     const { id: tweetId } = req.params;
-    const tweet = await manageReadTweet(tweetId).catch((error) => 
-        res.status(400).json({error: error.message}));
+    const tweet = await manageReadTweet(tweetId).catch(errorHandler(res, 400));
     if(tweet) 
-        res.status(200).json(tweet);
-    else 
-        res.status(404).json({ message: "Tweet could not be read (in controlReadR=Tweet)" });
+        sucessHandler(res, 'Read 1 tweet (in controller)', tweet, 200);
 };
 
 // Update
 export const controlUpdateTweet = async (req: Request, res: Response) => {
     const { id: tweetId } = req.params;
     const updateData = req.body as Partial<TweetType>;
-    const updatedTweet = await manageUpdateTweet(tweetId, updateData).catch((error) => 
-        {res.status(400).json({ error: error.message })});
+    const updatedTweet = await manageUpdateTweet(tweetId, updateData).catch(errorHandler(res, 400));
     if(updatedTweet) 
-        res.status(200).json(updatedTweet);
-    else 
-        res.status(404).json({ message: "Tweet could not be updated (in controlUpdateTweet)" });
+        sucessHandler(res, 'Updated 1 tweet (in controller)', updatedTweet, 200);
 };
 
 // Delete
 export const controlDeleteTweet = async (req: Request, res: Response) => {
     const { id: tweetId } = req.params;
-    const deleteTweetResult = await manageDeleteTweet(tweetId).catch((error) => 
-        res.status(400).json({ error: error.message }));
+    const deleteTweetResult = await manageDeleteTweet(tweetId).catch(errorHandler(res, 400));
     if(deleteTweetResult)
-        res.status(200).json({ message: "Tweet deleted successfully (in controlDeleteTweet)" });
-    else
-        res.status(404).json({ message: "Tweet could not be deleted (in controlDeleteTweet" });
-
+        sucessHandler(res, 'Deleted 1 tweet (in controller)', deleteTweetResult, 200);
 }
 
