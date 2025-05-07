1af504306068422ad4b4b637637930bf07f6be75
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-07 15:53:50 +0300

    made functions exports

--- TwitterService/Lib/handler/sucess.handler.ts
+++ TwitterService/Lib/handler/sucess.handler.ts
@@ -1,4 +1,4 @@
 import { Response } from "express";
 
-const handleSuccess = (res: Response, message: string, data: any, statCode: number) => 
+export const sucessHandler = (res: Response, message: string, data: any, statCode: number) => 
     res.status(statCode).json({ message: `${message}: ${JSON.stringify(data)}` });
