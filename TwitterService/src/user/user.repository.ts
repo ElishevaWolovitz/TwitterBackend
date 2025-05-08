fb08592a0c1ed758474cf9fb1699047770b8f478
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-08 10:07:23 +0300

    refactored code to make neater and shorter

--- TwitterService/src/user/user.repository.ts
+++ TwitterService/src/user/user.repository.ts
@@ -1,22 +1,25 @@
 import { UserModel }from "../../Lib/models/user.model";
-import { Types } from "mongoose";
 import { UserType } from "../../Lib/types/user.types";
 
 
 //Create/Post
 export const reposCreateUser = async (user: UserType): Promise<UserType> => {
-    const newUser = new UserModel(user);
-    await newUser.save();
-    if(!newUser)
-        throw new Error('User could not be created in DB (in reposCreateUser)');
-    return newUser; 
+    return await new UserModel(user).save();
+    //Q: was not sure if i should do error handling like what is commented below or if the above is better and simpler? 
+    // const newUser = new UserModel(user);
+    // await newUser.save();
+    // if(!newUser)
+    //     throw new Error('User could not be created in DB (in reposCreateUser)');
+    // return newUser; 
 };
 
-//Read All/Get All
 //Q: Is the return type here correct? 
+//Read All/Get All
 export const reposReadAllUsers = async (): Promise<UserType[]>  => {
-    const readUsers = await UserModel.find();
-    if(!readUsers)
-        throw new Error('Users not found in DB (in reposReadAllUsers)');
-    return readUsers;
+    return await UserModel.find();
+    //Q: was not sure if i should do error handling like what is commented below or if the above is better and simpler? 
+    // const readUsers = await UserModel.find();
+    // if(!readUsers)
+    //     throw new Error('Users not found in DB (in reposReadAllUsers)');
+    // return readUsers;
 }
