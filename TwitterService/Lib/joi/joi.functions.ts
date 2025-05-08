b5e67ccdadf7a689568bc75020550962170abcc5
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-08 09:36:49 +0300

    changed to fit conventions of nammed functions

--- TwitterService/Lib/joi/joi.functions.ts
+++ TwitterService/Lib/joi/joi.functions.ts
@@ -1,15 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 import Joi from 'joi';
 
-
-const runValidation = (
-  schema: Joi.ObjectSchema,
-  data: any,
-  res: Response,
-  next: NextFunction
-): void => {
+//Q3: Should the type for data not be any? 
+const runValidation = (schema: Joi.ObjectSchema, data: any, res: Response, next: NextFunction ): void => {
   const { error } = schema.validate(data, { abortEarly: false });
-
   if (error) {
     const errorMessages = error.details.map(getErrorMessage);
     res.status(400).json({ error: errorMessages });
@@ -20,14 +14,14 @@ const runValidation = (
 
 const getErrorMessage = (detail: Joi.ValidationErrorItem): string => detail.message;
 
-export const validateBody = (schema: Joi.ObjectSchema) =>
-//Q: I dont really understand why here it needs to be a function in a function why can it not be 
-//   (schema: Jpo.ObjectSchema, req: Request, res: Response, next: NextFunction) => void { runValidation(...)}
-  function validateRequestBody(req: Request, res: Response, next: NextFunction): void {
+export const validateBody = (schema: Joi.ObjectSchema) => {
+    const validateRequestBody = (req: Request, res: Response, next: NextFunction): void => 
     runValidation(schema, req.body, res, next);
-  };
+    return validateRequestBody;
+};
 
-export const validateParams = (schema: Joi.ObjectSchema) =>
-  function validateRequestParams(req: Request, res: Response, next: NextFunction): void {
+export const validateParams = (schema: Joi.ObjectSchema) => {
+    const validateRequestParams = (req: Request, res: Response, next: NextFunction): void =>
     runValidation(schema, req.params, res, next);
+    return validateRequestParams;
   };
