5e99f44e23d1f5da4d41180b0d1719463679e20e
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-08 10:52:29 +0300

    fixed spelling mistake

--- TwitterService/src/user/user.validator.ts
+++ TwitterService/src/user/user.validator.ts
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
 export const userBodySchema = Joi.object({
-    userName: Joi.string().required(),
+    username: Joi.string().required(),
     displayName: Joi.string().required()
 })
