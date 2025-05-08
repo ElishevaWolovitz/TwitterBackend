f8b8d34f386534ab1439a58ba9c604c1a1159f7b
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-08 11:01:42 +0300

    deleted unsued files

--- TwitterService/Lib/handle.results/error.handling.ts
+++
@@ -1,4 +0,0 @@
-import { Response } from "express";
-
-const handleError = (res: Response, statusCode: number) =>
-  (error: Error) => res.status(statusCode).json({ error: error.message });
--- TwitterService/Lib/handle.results/sucess.handling.ts
+++
@@ -1,4 +0,0 @@
-import { Response } from "express";
-
-const handleSuccess = (res: Response, message: string, data: any) => 
-    res.status(200).json({ message: `${message}: ${JSON.stringify(data)}` });
