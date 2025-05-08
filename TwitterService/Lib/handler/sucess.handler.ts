f82397b1fbc8d3032db003f89dda3b57f2e3abbd
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-08 10:52:12 +0300

    changes to make print message neater

--- TwitterService/Lib/handler/sucess.handler.ts
+++ TwitterService/Lib/handler/sucess.handler.ts
@@ -1,4 +1,4 @@
 import { Response } from "express";
 
 export const sucessHandler = (res: Response, message: string, data: any, statCode: number) => 
-    res.status(statCode).json({ message: `${message}: ${JSON.stringify(data)}` });
+    res.status(statCode).json({ message: message, data: data });
