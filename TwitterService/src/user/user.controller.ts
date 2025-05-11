28590cd768e1bd39a353492f5cf919fac9a29f1f
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-11 15:22:37 +0300

    answered comment

--- TwitterService/src/user/user.controller.ts
+++ TwitterService/src/user/user.controller.ts
@@ -8,7 +8,6 @@ import { UserType } from "../types/user.types";
 import { errorHandler } from '../handler/error.handler';
 import { successHandler } from '../handler/success.handler';
 
-//Q9: Do all functions in manager and repository also have to be async and await? or just here in controller?  
 // Create
 export const controlCreateUser = async (req: Request, res: Response) => {
     const user = new UserModel(req.body as UserType);
